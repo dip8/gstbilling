78286be9f5595a74042c52c36da6fbbf3499ed43
dip8 <dipaklokhande81@gmail.com>
2025-04-15 21:36:00 +0530

    Initial Commit with mail

---
+++ src/main/java/com/gstbilling/GstbillingApplication.java
@@ -0,0 +1,13 @@
+package com.gstbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstbillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstbillingApplication.class, args);
+	}
+
+}
